646de930bda2daa362757d5ec8c9d37b91c6fa66
ss-savari <sorensavari2007@gmail.com>
2024-11-26 21:45:36 -0500

    Javscript
    The javascript part of the project

---
+++ javascript shit.js
@@ -0,0 +1,9 @@
+// Optional: Add functionality if you want scroll effects
+document.querySelectorAll('.hexagon a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
